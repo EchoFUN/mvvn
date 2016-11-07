20c41697ed2018eb4d286806e1b5e79bbb873ff0
EchoFUN <xukai.ken@gmail.com>
2016-11-07 20:08:10 +0800

    Add.

---
+++ build/server.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @fileOverview 开发服务器程序，启动整体的开发服务；
+ * @date 2016-11-07 星期一
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const Koa = require('koa');
+const serve = require('koa-static');
+
+const app = Koa();
+
+app.use(serve(__dirname + '/../demo'));
+app.listen('8080');
